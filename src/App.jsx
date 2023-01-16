233ff623c6c097e84850c3581a6e5503dbac3e59
kemal kujovic <kemalkujovic1111@gmail.com>
2023-01-16 20:15:11 +0100

    first-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import StyleButton from "./components/styleButton/styleButton";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
+        <StyleButton />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/styleButton/styleButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styleButton.css";
+const StyleButton = () => {
+  return <button className="styleButton">Click me</button>;
+};
+
+export default StyleButton;
