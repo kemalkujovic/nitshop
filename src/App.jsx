bfcf560c9be0601c1673de955ed6bcef4ba53704
kemal kujovic <kemalkujovic1111@gmail.com>
2023-01-23 20:49:04 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import logo from "./logo.svg";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import StyleButton from "./components/styleButton/styleButton";
-import StyleHeader from "./components/styleHeader/styleHeader";
-function App() {
-  const [innerText, setInnerText] = useState("Clicked");
-  const [clicked, setClicked] = useState(false);
-  useEffect(() => {
-    clicked ? setInnerText("Clicked") : setInnerText("Click me");
-  }, [clicked]);
-  function buttonClick() {
-    setClicked(!clicked);
-  }
+import CustomForm from "./components/customForm/customForm";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <StyleHeader text="This is header" />
-        <StyleButton onClickHandler={buttonClick} innerText={innerText} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <CustomForm />
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/styleHeader/styleHeader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styleHeader.css";
-
-const StyleHeader = (props) => {
-  return <h3 className="headerText">{props.text}</h3>;
-};
-
-export default StyleHeader;
