14b866495ee9e6dc3063dce670c076110e50eb25
kemal kujovic <kemalkujovic1111@gmail.com>
2023-02-22 17:13:37 +0100

    fixed-light

--- src/util/theme.js
+++ src/util/theme.js
@@ -7,7 +7,7 @@ export const colors = {
   lightGrayColor: "#b3b1b1",
 };
 export const fontWeight = {
-  light: "200",
+  light: "100",
   medium: "400",
   mediumBold: "600",
   bold: "800",
@@ -16,7 +16,7 @@ export const fontWeight = {
 export const fontSize = {
   xSmall: "10px",
   small: "12px",
-  smallPlus: '14px',
+  smallPlus: "14px",
   normal: "16px",
   medium: "18px",
   large: "24px",
