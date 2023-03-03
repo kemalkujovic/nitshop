b38c811c58994efaa54d9b48095fba9412b427c9
kemal kujovic <kemalkujovic1111@gmail.com>
2023-03-03 20:03:41 +0100

    add Hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import Navigation from "./Templates/Navigation/Navigation";
 import Slider from "./Templates/Slider/Slider";
+import SimplifiedDiv from "./components/SimplifiedDiv/SimplifiedDiv";
 const App = () => {
   return (
     <>
-      <Navigation />
-      <Slider />
+        <Navigation />
+        <Slider />
     </>
   );
 };
--- src/Templates/Navigation/LinkBar.jsx
+++ src/Templates/Navigation/LinkBar.jsx
@@ -4,6 +4,8 @@ import CustomDiv from "../../components/CustomDiv/CustomDiv";
 import { colors, fontSize } from "../../util/theme";
 import Text from "../../components/Text/Text";
 import SearchBar from "./SearchBar";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import { getScreenWidth } from "../../util/helper";
 const styles = {
   navBar: {
     display: "flex",
@@ -12,58 +14,83 @@ const styles = {
     height: "30px",
   },
 };
-const LinkBar = (props) => {
-  return (
-    <>
-      <CustomDiv
-        bgColor="white"
-        width="100%"
-        height="auto"
-        border="0px"
-        display="flex"
-        padding="0px 10%"
-      >
-        <Grid container direction="row">
-          <Grid item lg={6} display="flex">
-            <CustomDiv margin="0px 15px 0px 0px">
-              <Text fontSize={fontSize.large} color={colors.orange}>
-                Home
-              </Text>
-            </CustomDiv>
-            <CustomDiv margin="0px 15px 0px 0px">
-              <Text fontSize={fontSize.large} color={colors.grayColor}>
-                Shop
-              </Text>
-            </CustomDiv>
-            <CustomDiv margin="0px 15px 0px 0px">
-              <Text fontSize={fontSize.large} color={colors.grayColor}>
-                Blog
-              </Text>
-            </CustomDiv>
-            <CustomDiv margin="0px 15px 0px 0px">
-              <Text fontSize={fontSize.large} color={colors.grayColor}>
-                O nama
-              </Text>
-            </CustomDiv>
-            <CustomDiv margin="0px 15px 0px 0px">
-              <Text fontSize={fontSize.large} color={colors.grayColor}>
-                Kontakt
-              </Text>
-            </CustomDiv>
-          </Grid>
-          <Grid item lg={6}>
-            <CustomDiv
-              display="flex"
-              alignItems="center"
-              justifyContent="flex-end"
-              height="100%"
-            >
-              <SearchBar />
-            </CustomDiv>
-          </Grid>
+const LinkBarLG = () => (
+  <>
+    <CustomDiv
+      bgColor="white"
+      width="100%"
+      height="auto"
+      border="0px"
+      display="flex"
+      padding="0px 10%"
+    >
+      <Grid container direction="row">
+        <Grid item lg={6} display="flex">
+          <CustomDiv margin="0px 15px 0px 0px">
+            <Text fontSize={fontSize.large} color={colors.orange}>
+              Home
+            </Text>
+          </CustomDiv>
+          <CustomDiv margin="0px 15px 0px 0px">
+            <Text fontSize={fontSize.large} color={colors.grayColor}>
+              Shop
+            </Text>
+          </CustomDiv>
+          <CustomDiv margin="0px 15px 0px 0px">
+            <Text fontSize={fontSize.large} color={colors.grayColor}>
+              Blog
+            </Text>
+          </CustomDiv>
+          <CustomDiv margin="0px 15px 0px 0px">
+            <Text fontSize={fontSize.large} color={colors.grayColor}>
+              O nama
+            </Text>
+          </CustomDiv>
+          <CustomDiv margin="0px 15px 0px 0px">
+            <Text fontSize={fontSize.large} color={colors.grayColor}>
+              Kontakt
+            </Text>
+          </CustomDiv>
         </Grid>
-      </CustomDiv>
-    </>
+        <Grid item lg={6}>
+          <CustomDiv
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-end"
+            height="100%"
+          >
+            <SearchBar />
+          </CustomDiv>
+        </Grid>
+      </Grid>
+    </CustomDiv>
+  </>
+);
+
+const LinkBarSM = () => (
+  <SimplifiedDiv
+    style={{
+      display: "block",
+      padding: "0px 10px",
+    }}
+  >
+    <SimplifiedDiv
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      
+    </SimplifiedDiv>
+  </SimplifiedDiv>
+);
+
+const LinkBar = (props) => {
+  const screenWidth = getScreenWidth();
+  return screenWidth === "SM" || screenWidth === "MD" ? (
+    <LinkBarSM />
+  ) : (
+    <LinkBarLG />
   );
 };
 
