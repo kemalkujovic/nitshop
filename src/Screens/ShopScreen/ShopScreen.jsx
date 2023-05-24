5f78860076265711a0d0fcff29e91a62861bb2ef
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-25 00:00:18 +0200

    router bug fixed

--- src/Screens/ShopScreen/ShopScreen.jsx
+++ src/Screens/ShopScreen/ShopScreen.jsx
@@ -1,57 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import Navigation from "../../Templates/Navigation/Navigation";
-import { CartContext } from "../../context/CartContext";
-import { UserContext } from "../../context/UserContext";
-import ProductsHomePage from "../../Templates/ProductsHomePage/ProductsHomePage";
-import Footer from "../../Templates/Footer/Footer";
-import CategoryTab from "../../Templates/CategoryTab/CategoryTab";
-import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
-import { Grid } from "@mui/material";
-import Text from "../../components/Text/Text";
-import { colors, fontSize, fontWeight } from "../../util/theme";
+
 import ArticleShopPage from "../../Templates/ArticleShopPage/ArticleShopPage";
 
 const ShopScreen = () => {
-  const { items } = useContext(CartContext);
-  const { user } = useContext(UserContext);
-  const styles = {
-    container: {
-      padding: "0px 10%",
-    },
-    headingContainer: {
-      display: "flex",
-      width: "100%",
-      textAlign: "center",
-      justifyContent: "center",
-      padding: "10px 0px",
-    },
-    headingText: {
-      fontSize: fontSize.large,
-      color: colors.accentColor,
-      fontWeight: fontWeight.mediumBold,
-    },
-  };
-
   return (
     <>
       <Navigation />
-      <SimplifiedDiv style={styles.container}>
-        <Grid container direction="row" spacing={5}>
-          <Grid item md={3} lg={3} xs={12} sm={12}>
-            <SimplifiedDiv style={styles.headingContainer}>
-              <Text style={styles.headingText}>Category</Text>
-            </SimplifiedDiv>
-            <CategoryTab />
-          </Grid>
-        <Grid item md={9} lg={9} xs={12} sm={12}>
-          <SimplifiedDiv style={styles.headingContainer}>
-            <Text style={styles.headingText}>Articles</Text>
-          </SimplifiedDiv>
-          <ArticleShopPage />
-        </Grid>
-        </Grid>
-      </SimplifiedDiv>
-      <Footer />
+      <ArticleShopPage />
     </>
   );
 };
--- src/Templates/ArticleShopPage/ArticleShopPage.jsx
+++ src/Templates/ArticleShopPage/ArticleShopPage.jsx
@@ -6,31 +6,64 @@ import ArticleCard from "../ArticleCard/ArticleCard";
 import article1 from "../../assets/images/home/product1.jpg";
 import article2 from "../../assets/images/home/product2.jpg";
 import article3 from "../../assets/images/home/product3.jpg";
+import { colors, fontSize, fontWeight } from "../../util/theme";
+import CategoryTab from "../../Templates/CategoryTab/CategoryTab";
+import Text from "../../components/Text/Text";
 const ArticleShopPage = () => {
+  const styles = {
+    container: {
+      padding: "0px 10%",
+    },
+    headingContainer: {
+      display: "flex",
+      width: "100%",
+      textAlign: "center",
+      justifyContent: "center",
+      padding: "10px 0px",
+    },
+    headingText: {
+      fontSize: fontSize.large,
+      color: colors.accentColor,
+      fontWeight: fontWeight.mediumBold,
+    },
+  };
   const [articles, setArticles] = useState([]);
   const images = [article1, article2, article3];
 
-  // useEffect(() => {
-  //   fetchAllArticle().then((allUsers) => setArticles(allUsers));
-  // }, []);
+  useEffect(() => {
+    fetchAllArticle().then((allUsers) => setArticles(allUsers));
+  }, []);
   return (
-    <SimplifiedDiv style={{}}>
-      <Grid container item direction="row" spacing={3}>
-        {articles.map((el, index) => {
-          const randomNum = Math.floor(Math.random() * 3);
-          return (
-            <Grid item lg={4} md={4}>
-              <ArticleCard
-                key={el.title}
-                title={el.title}
-                image={images[randomNum]}
-                price="56"
-              />
-            </Grid>
-          );
-        })}
+    <Grid container style={styles.container}>
+      <Grid item md={3} lg={3} xs={12} sm={12}>
+        <SimplifiedDiv style={styles.headingContainer}>
+          <Text style={styles.headingText}>Category</Text>
+        </SimplifiedDiv>
+        <CategoryTab />
       </Grid>
-    </SimplifiedDiv>
+      <Grid item md={9} lg={9} xs={12} sm={12}>
+        <SimplifiedDiv style={styles.headingContainer}>
+          <Text style={styles.headingText}>Articles</Text>
+        </SimplifiedDiv>
+        <SimplifiedDiv style={{}}>
+          <Grid container item direction="row" spacing={3}>
+            {articles.map((el) => {
+              const randomNum = Math.floor(Math.random() * 3);
+              return (
+                <Grid item lg={4} md={4}>
+                  <ArticleCard
+                    key={el.title}
+                    title={el.title}
+                    image={images[randomNum]}
+                    price="56"
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </SimplifiedDiv>
+      </Grid>
+    </Grid>
   );
 };
 
